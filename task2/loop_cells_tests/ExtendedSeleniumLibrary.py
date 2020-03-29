3ba356f9c63a3329be10b2b56701e8f7044da17a
Janne Parpala <paja@kapsi.fi>
2020-03-29 21:58:52 +0300

    Styling changes

--- task2/loop_cells_tests/ExtendedSeleniumLibrary.py
+++ task2/loop_cells_tests/ExtendedSeleniumLibrary.py
@@ -1,12 +1,12 @@
 from robot.libraries.BuiltIn import BuiltIn
 
-def get_table_column_cell_values(searched_column_text = ''):
+def get_table_column_cell_values(searched_column_text=''):
     """Get table column values from certain header to list"""
     seleniumlib = BuiltIn().get_library_instance('SeleniumLibrary')
 
     # Get column index from the searched header
     table_columns_count = seleniumlib.get_element_count('xpath://table/thead/tr/th')
-    for i in range(1,table_columns_count + 1):
+    for i in range(1, table_columns_count + 1):
         text = seleniumlib.get_text('xpath://table/thead/tr/th[%s]' % i)
         if text == searched_column_text:
             searched_column_index = i
